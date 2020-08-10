2bf9c575f7fdecc319c74861184ae6c65c4b4aea
James Kelly <jek030@bucknell.edu>
2020-08-10 19:21:52 -0400

    connected to api, got stock info

--- financeGuy.py
+++ financeGuy.py
@@ -1,4 +1,5 @@
 from tiingo import TiingoClient
+import numpy as np
 # Set TIINGO_API_KEY in your environment variables in your .bash_profile, OR
 # pass a dictionary with 'api_key' as a key into the TiingoClient.
 
@@ -18,3 +19,15 @@ ticker_metadata = client.get_ticker_metadata("GOOGL")
 
 #print(ticker_metadata)
 ticker_history = client.get_dataframe("GOOGL")
+#print(ticker_history)
+
+apple = client.get_ticker_price("AAPL", fmt='json', 
+startDate='2019-08-07', endDate='2020-08-07', frequency='annually')
+
+google = client.get_ticker_price("GOOGL", fmt='json', 
+startDate='2019-08-07', endDate='2020-08-07', frequency='annually')
+
+for row in historical_prices:
+    print(row['date'], row['close'])
+
+#historical_prices.to_html('index.html')
