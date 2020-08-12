1f403664036a3a3f2f1ac23211e3af30879cb5cb
James Kelly <jek030@bucknell.edu>
2020-08-12 15:51:27 -0400

    YTD works

--- financeGuy.py
+++ financeGuy.py
@@ -1,5 +1,7 @@
 from tiingo import TiingoClient
 import numpy as np
+import pandas as pd
+from io import StringIO
 # Set TIINGO_API_KEY in your environment variables in your .bash_profile, OR
 # pass a dictionary with 'api_key' as a key into the TiingoClient.
 
@@ -15,19 +17,48 @@ config['api_key'] = "a17ef094e195b4a09409f0fb1753880fc420154a"
 # Initialize
 client = TiingoClient(config)
 
-ticker_metadata = client.get_ticker_metadata("GOOGL")
+########################################################
+# Start of our code ####################################
+########################################################
+########################################################
 
-#print(ticker_metadata)
-ticker_history = client.get_dataframe("GOOGL")
-#print(ticker_history)
+eight_stocks = ["AAPL","GOOGL","MSFT", "AMD", "PYPL","SQ", "TSLA","TMXLF"]
 
-apple = client.get_ticker_price("AAPL", fmt='json', 
-startDate='2019-08-07', endDate='2020-08-07', frequency='annually')
+frame = []
 
-google = client.get_ticker_price("GOOGL", fmt='json', 
-startDate='2019-08-07', endDate='2020-08-07', frequency='annually')
+for ticker in eight_stocks:
+    jsonData = client.get_ticker_price(ticker, fmt='json', startDate='2019-08-07',  frequency='daily')
+   # print("***************")
+   # print(jsonData)
+    #here is where u take the difference. each 1st row is the startDate infp and 2nd is the endDate info
+    lastYearPrice = 0.0
+    thisYearPRice = 0.0
+    i =0
+    for row in jsonData:
+        #print("STOCK: ", ticker,  row['close'])
+        if i == 0:
+            lastYearPrice = row['close']
+        
+        thisYearPRice = row['close']
+        i += 1
+     #% return formula here
+    percentReturn = (thisYearPRice / lastYearPrice) * 100
+  
+    singleStockDataFrame = pd.DataFrame({'Ticker': [ticker], '% Return': [percentReturn]} )
+    frame.append(singleStockDataFrame)
+    #print()  
 
-for row in apple:
-    print(row['date'], row['close'])
+YTD_df = pd.concat(frame)
 
-#historical_prices.to_html('index.html')
+YTD_df = YTD_df.sort_values(by= ['% Return'], ignore_index=True, ascending=False)
+# turn data frame to html text
+YTD_df.to_html('index.html')
+print(YTD_df)
+
+
+#ticker_history = client.get_dataframe(eight_stocks,
+#                                      frequency='daily',
+##                                      metric_name='close',
+#                                      startDate='2019-08-07',
+#                                      endDate='2020-08-07')
+
