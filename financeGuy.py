dabc9c451f01fb2ba5fb0ea1ee04e059e5a1b936
Peter Andresen <peterandresen@Peters-MacBook-Pro-2.local>
2020-09-25 23:51:26 -0400

    added dictionary of key-value pairs for 5yr function

--- financeGuy.py
+++ financeGuy.py
@@ -17,17 +17,17 @@ def main():
 
     # If you don't have your API key as an environment variable,
     # pass it in via a configuration dictionary.
-    config['api_key'] = "a17ef094e195b4a09409f0fb1753880fc420154a"
-    #config['api_key'] = "028f16053ce2180643cb2443b4b24736967452c1"
+    #config['api_key'] = "a17ef094e195b4a09409f0fb1753880fc420154a"
+    config['api_key'] = "028f16053ce2180643cb2443b4b24736967452c1"
 
 
 
     # Initialize
     client = TiingoClient(config)
     # --START OF OUR CODE--
-    SP500 = getTickers()
-    #createLastYearDF(top100, client)
-    create5YearDF(SP500, client)
+    top100 = getTickers()
+    #createLastYearDF(SP500, client)
+    create5YearDF(top100, client)
     #create10YearDF(top100, client)
 
 def getTickers():
@@ -35,13 +35,13 @@ def getTickers():
     SP500 = ['MMM','ABT','ABBV','ABMD','ACN','ATVI','ADBE','AMD','AAP','AES','AFL','A','APD','AKAM','ALK','ALB','ARE','ALXN','ALGN','ALLE','LNT','ALL','GOOGL','GOOG','MO','AMZN','AMCR','AEE','AAL','AEP','AXP','AIG','AMT','AWK','AMP','ABC','AME','AMGN','APH','ADI','ANSS','ANTM','AON','AOS','APA','AIV','AAPL','AMAT','APTV','ADM','ANET','AJG','AIZ','T','ATO','ADSK','ADP','AZO','AVB','AVY','BKR','BLL','BAC','BK','BAX','BDX','BRK-B','BBY','BIO','BIIB','BLK','BA','BKNG','BWA','BXP','BSX','BMY','AVGO','BR','BF-B','CHRW','COG','CDNS','CPB','COF','CAH','KMX','CCL','CARR','CAT','CBOE','CBRE','CDW','CE','CNC','CNP','CTL','CERN','CF','SCHW','CHTR','CVX','CMG','CB','CHD','CI','CINF','CTAS','CSCO','C','CFG','CTXS','CLX','CME','CMS','KO','CTSH','CL','CMCSA','CMA','CAG','CXO','COP','ED','STZ','COO','CPRT','GLW','CTVA','COST','COTY','CCI','CSX','CMI','CVS','DHI','DHR','DRI','DVA','DE','DAL','XRAY','DVN','DXCM','FANG','DLR','DFS','DISCA','DISCK','DISH','DG','DLTR','D','DPZ','DOV','DOW','DTE','DUK','DRE','DD','DXC','ETFC','EMN','ETN','EBAY','ECL','EIX','EW','EA','EMR','ETR','EOG','EFX','EQIX','EQR','ESS','EL','EVRG','ES','RE','EXC','EXPE','EXPD','EXR','XOM','FFIV','FB','FAST','FRT','FDX','FIS','FITB','FE','FRC','FISV','FLT','FLIR','FLS','FMC','F','FTNT','FTV','FBHS','FOXA','FOX','BEN','FCX','GPS','GRMN','IT','GD','GE','GIS','GM','GPC','GILD','GL','GPN','GS','GWW','HRB','HAL','HBI','HIG','HAS','HCA','PEAK','HSIC','HSY','HES','HPE','HLT','HFC','HOLX','HD','HON','HRL','HST','HWM','HPQ','HUM','HBAN','HII','IEX','IDXX','INFO','ITW','ILMN','INCY','IR','INTC','ICE','IBM','IP','IPG','IFF','INTU','ISRG','IVZ','IPGP','IQV','IRM','JKHY','J','JBHT','SJM','JNJ','JCI','JPM','JNPR','KSU','K','KEY','KEYS','KMB','KIM','KMI','KLAC','KSS','KHC','KR','LB','LHX','LH','LRCX','LW','LVS','LEG','LDOS','LEN','LLY','LNC','LIN','LYV','LKQ','LMT','L','LOW','LYB','MTB','MRO','MPC','MKTX','MAR','MMC','MLM','MAS','MA','MKC','MXIM','MCD','MCK','MDT','MRK','MET','MTD','MGM','MCHP','MU','MSFT','MAA','MHK','TAP','MDLZ','MNST','MCO','MS','MOS','MSI','MSCI','MYL','NDAQ','NOV','NTAP','NFLX','NWL','NEM','NWSA','NWS','NEE','NLSN','NKE','NI','NBL','NSC','NTRS','NOC','NLOK','NCLH','NRG','NUE','NVDA','NVR','ORLY','OXY','ODFL','OMC','OKE','ORCL','OTIS','PCAR','PKG','PH','PAYX','PAYC','PYPL','PNR','PBCT','PEP','PKI','PRGO','PFE','PM','PSX','PNW','PXD','PNC','PPG','PPL','PFG','PG','PGR','PLD','PRU','PEG','PSA','PHM','PVH','QRVO','PWR','QCOM','DGX','RL','RJF','RTX','O','REG','REGN','RF','RSG','RMD','RHI','ROK','ROL','ROP','ROST','RCL','SPGI','CRM','SBAC','SLB','STX','SEE','SRE','NOW','SHW','SPG','SWKS','SLG','SNA','SO','LUV','SWK','SBUX','STT','STE','SYK','SIVB','SYF','SNPS','SYY','TMUS','TROW','TTWO','TPR','TGT','TEL','FTI','TDY','TFX','TXN','TXT','TMO','TIF','TJX','TSCO','TT','TDG','TRV','TFC','TWTR','TYL','TSN','UDR','ULTA','USB','UAA','UA','UNP','UAL','UNH','UPS','URI','UHS','UNM','VFC','VLO','VAR','VTR','VRSN','VRSK','VZ','VRTX','VIAC','V','VNO','VMC','WRB','WAB','WMT','WBA','DIS','WM','WAT','WEC','WFC','WELL','WST','WDC','WU','WRK','WY','WHR','WMB','WLTW','WYNN','XEL']
     #,'XRX','XLNX','XYL','YUM','ZBRA','ZBH','ZION','ZTS'   ** took this off the end so it's less than 500 calls
    
-    #return top100
-    return SP500
+    return top100
+    #return SP500
     
 
 
 #1
-def createLastYearDF(top100, client):
+def createLastYearDF(SP500, client):
     listOfDFs = [] #a list to temporarily hold each stocks DF so we can combine them
     today = date.today()
     thisYear = today.strftime("%Y")
@@ -115,20 +115,23 @@ def createLastYearDF(top100, client):
 
 
 #2
-def create5YearDF(SP500, client):
+def create5YearDF(top100, client):
     listOfDFs = [] #a list to temporarily hold each stocks DF so we can combine them
     today = date.today()
     thisYear = today.strftime("%Y")
     lastYear = int(thisYear) - 5
     lastYear = str(lastYear)
+    tickers = {}
 
-    for ticker in SP500:
+    for ticker in top100:
         jsonData = client.get_ticker_price(ticker, fmt='json', startDate= lastYear+today.strftime('-%m-%d'),  frequency='daily') # gets data fromAPI in JSON format
 
         #here is where u take the difference. each 1st row is the startDate infp0and 2nd is the endDate info
         lastYearPrice = 0.0
         thisYearPrice = 0.0
         i =0
+        # need to make a diciontary with ticker-return as key-value pairs
+
         for row in jsonData:
             #print("STOCK: ", ticker,  row['close'])
             if i == 0:
@@ -137,15 +140,21 @@ def create5YearDF(SP500, client):
             thisYearPrice = row['close'] #keep iterating until most recent price
             i += 1
         percentReturn = (thisYearPrice / lastYearPrice) * 100 # percent return formula here
-    
+
+        
+        tickers[ticker] = percentReturn
+        
+
         singleStockDataFrame = pd.DataFrame({'Ticker': [ticker], '5 Year Return (%)': [percentReturn]} ) #create a DF of each stock with its ticker and PR
         listOfDFs.append(singleStockDataFrame) #append each DF to the list of DFs
     
     FiveYTD_df = pd.concat(listOfDFs) #Turn list of data frames into one data frame
     FiveYTD_df = FiveYTD_df.sort_values(by= ['5 Year Return (%)'], ignore_index=True, ascending=False) #order the DF by highest PR
     FiveYTD_df.index = FiveYTD_df.index +1
-
     
+    #sorts tickers diciontary by percent return
+    tickers = sorted(tickers.items(), key = lambda x: x[1], reverse = True)
+    print(tickers)
     pd.set_option('colheader_justify', 'center')   # FOR TABLE <th> what does this do
     html_string = '''
     <link rel="stylesheet" type="text/css" href="df_style.css"/>
